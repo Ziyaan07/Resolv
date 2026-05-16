21ee94a3c5c0cba366c446966558b215d57b51b8
Ziyaan Kashif <itsmeziyaan1@gmail.com>
2026-05-17 01:31:26 +0500

    chore: move standalone scripts to scripts/ directory

--- scripts/create_bucket.js
+++ scripts/create_bucket.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
 const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
